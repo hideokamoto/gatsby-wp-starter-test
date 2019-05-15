6f1b516c3a2d06724899d1cf6861926baa3dca0d
hideokamoto <hide.okamoto@digitalcube.jp>
2019-05-15 16:25:55 +0900

    init

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby + WordPress Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        // The base url to your WP site.
+        baseUrl: '8cd047fc-b77d-4888-a6b4-50840022a515.appdev.getshifter.io:20119',
+        // WP.com sites set to true, WP.org set to false
+        hostingWPCOM: false,
+        // The protocol. This can be http or https.
+        protocol: 'https',
+        // Use 'Advanced Custom Fields' Wordpress plugin
+        useACF: false,
+        auth: {},
+        // Set to true to debug endpoints on 'gatsby build'
+        verboseOutput: true,
+        searchAndReplaceContentUrls: {
+          sourceUrl: 'https://8cd047fc-b77d-4888-a6b4-50840022a515.appdev.getshifter.io:20119',
+          replacementUrl: 'https://optimistic-heyrovsky226.on.getshifter.io',
+        },
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      // Removes unused css rules
+      resolve:'gatsby-plugin-purgecss',
+      options: {
+        // Activates purging in gatsby develop
+        develop: true,
+        // Purge only the main css file
+        purgeOnly: ['/all.sass'],
+      },
+    }, // must be after other CSS plugins
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
+  ],
+}
